3d747742642ca6738d04edf32a3ac58d349bd277
Angus <donald.macisaac@mail.mcgill.ca>
2015-05-22 13:48:28 -0400

    Fix bug when forceDisconnecting streams that you are not subscribed to.

--- src/services/data-manager.js
+++ src/services/data-manager.js
@@ -9,6 +9,7 @@
         self.subscribers = [];
         self.streamsAvailable = [];
 
+        /* Creates a new subscriber object and adds it to the list of subscribers */
         self.createSubscriber = function() {
             var subscriber = new Subscriber(self.subscribers.length + 1);
 
@@ -18,13 +19,23 @@
             return subscriber;
         };
 
+        /* Returns true if the stream is being subscribed to */
         self.isBeingSubscribedTo = function(stream) {
+            if (!stream) {
+                return;
+            }
+
             return self.subscribers.some(function (s) {
                 return s.session && s.session.stream && s.session.stream.id == stream.id;
             });
         };
 
+        /* Removes the stream if it exists and sets a new fullscreen stream */
         self.removeSubscriberByStream = function (stream) {
+            if (!stream) {
+                return;
+            }
+
             self.subscribers = self.subscribers.filter(function (s) {
                 return s.session.stream && s.session.stream.id != stream.id;
             });
@@ -32,7 +43,12 @@
             self.switchFullscreen();
         };
 
+        /* Returns a stream associated with the connection if it exists */
         self.getStreamByConnection = function (connection) {
+            if (!connection) {
+                return;
+            }
+
             var streams = self.streamsAvailable.filter(function (s) {
                 return s.connection.id === connection.id;
             });
@@ -40,7 +56,12 @@
             return streams.length ? streams[0] : null;
         };
 
+        /* Removes a stream associated with the connection if it exists */
         self.removeStreamByConnection = function (connection) {
+            if (!connection) {
+                return;
+            }
+
             var stream = self.getStreamByConnection(connection);
 
             if (stream) {
@@ -54,6 +75,7 @@
             self.switchFullscreen();
         };
 
+        /* Reorders the thumbnails based on who is fullscreen */
         self.switchFullscreen = function (subscriber) {
             if (subscriber && subscriber.isFullscreen) {
                 return;
